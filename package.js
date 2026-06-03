24c87087780b87870950e4617a64da5ce2d9c4ef
Misbah279-creator <taniyashahid38@gmail.com>
2026-06-04 02:14:55 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "shopnest",
+  "version": "1.0.0",
+  "description": "ShopNest E-Commerce Website",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.19.2"
+  }
+}
